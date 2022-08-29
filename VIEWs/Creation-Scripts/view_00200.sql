847552bbf3c12f6c746406ec2391200c20afd914
Condensator <email>
2022-08-29 11:59:22 +0300

    views

---
+++ VIEWs/Creation-Scripts/view_00200.sql
@@ -0,0 +1,7 @@
+create ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `view_00200` as 
+
+select `actor`.`actor_id` as `actor_id`,
+	`actor`.`first_name` as `first_name`,
+	`actor`.`last_name` as `last_name`,
+	`actor`.`last_update` as `last_update` 
+from `actor`
