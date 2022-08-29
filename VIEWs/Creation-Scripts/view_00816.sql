c41cf87f8ae8f32aa0204819253de70332e04021
Condensator <email>
2022-08-29 13:07:15 +0300

    123

--- VIEWs/Creation-Scripts/view_00816.sql
+++
@@ -1,7 +0,0 @@
-create ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `view_00816` as 
-
-select `actor`.`actor_id` as `actor_id`,
-	`actor`.`first_name` as `first_name`,
-	`actor`.`last_name` as `last_name`,
-	`actor`.`last_update` as `last_update` 
-from `actor`
